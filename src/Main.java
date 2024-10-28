5c5c7589a195d211fb992ba47047784bf612f569
KhanOfDenmark <daniilhanin2@mail.ru>
2024-10-28 20:06:43 +0300

    Corrected commit #2.

--- src/Main.java
+++ src/Main.java
@@ -75,13 +75,30 @@ public class Main {
         //Task#5
         System.out.println("Введите число месяца в году:");
         byte monthNumber = new Scanner(System.in).nextByte();
-        result = switch (monthNumber) {
-            case 1, 2, 3 -> "Зима";
-            case 4, 5, 6 -> "Весна";
-            case 7, 8, 9 -> "Лето";
-            case 10, 11, 12 -> "Осень";
-            default -> "Такого времени года не существует";
-        };
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                result = "Зима";
+                break;
+            case 3:
+            case 4:
+            case 5:
+                result = "Весна";
+                break;
+            case 6:
+            case 7:
+            case 8:
+                result = "Лето";
+                break;
+            case 9:
+            case 10:
+            case 11:
+                result = "Осень";
+                break;
+            default:
+                result = "Такого времени года не существует";
+        }
         System.out.println(monthNumber +"-й месяц принадлежит к сезону "
                 +result);
         System.out.println("***");
